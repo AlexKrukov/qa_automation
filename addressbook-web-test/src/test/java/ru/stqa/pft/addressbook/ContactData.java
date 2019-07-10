74298aa391dc5410306f1a4209eacd6b3f85718d
Alex Krukov <alexkrukov026@gmail.com>
2019-07-09 21:19:52 -0400

    Task #4 ContactCreationTests

---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String phone;
+    private final String email;
+
+    public ContactData(String firstName, String lastName, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
