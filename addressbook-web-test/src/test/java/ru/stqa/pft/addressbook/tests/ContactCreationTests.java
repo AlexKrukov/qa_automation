a3f8b4c31ee4afb46bd79522049de01402ac7df1
Alex Krukov <alexkrukov026@gmail.com>
2019-07-11 20:02:31 -0400

    2.7
    Created:
    - ContactHelper
    - GroupHelper
    - NavigationHelper
    - SessionHelper

---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase {
+
+
+    @Test
+    public void testContactCreation() {
+        app.getNavigationHelper().gotoHomePage();
+        app.getContactHelper().initContactCreation();
+        app.getContactHelper().fillContactForm(new ContactData("John", "Travolta", "6171112233", "jt777@gmail.com"));
+        app.getContactHelper().enterNewContact();
+        app.getContactHelper().returnToHomePage();
+    }
+
+}
