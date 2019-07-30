7e7b0b95de8a6f7658d4ba8b706b9db054d94ed0
Alex Krukov <alexkrukov026@gmail.com>
2019-07-30 06:00:25 -0400

    4.9 Lambdas (Groups)

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,18 +15,12 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation() {
         app.getNavigationHelper().gotoHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
-        ContactData contact = new ContactData("John", "Travolta", "6171112233", "jt777@gmail.com", "test1");
+        ContactData contact = new ContactData("Johnny2", "Travolta", "6171112233", "jt777@gmail.com", "test1");
         app.getContactHelper().createContact(contact, true);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (ContactData c : after) {
-            if (c.getId() > max) {
-                max = c.getId();
-            }
-        }
-        contact.setId(max);
+        contact.setId(after.stream().max(Comparator.comparingInt(o -> o.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
