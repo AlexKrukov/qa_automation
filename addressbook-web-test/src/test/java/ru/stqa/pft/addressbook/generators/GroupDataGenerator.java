45b0e6d6f1644825d1b356c161ba9afca9865d36
Alex Krukov <alexkrukov026@gmail.com>
2019-08-26 05:43:28 -0400

    6.5 Test data loading from file

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -32,9 +32,6 @@ public class GroupDataGenerator {
         }
         generator.run();
 
-        //int count = Integer.parseInt(args[0]);
-        //File file = new File(args[1]);
-
     }
 
     private void run() throws IOException {
--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.security.acl.Group;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,11 +17,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<>();
-        list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-        list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-        list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
